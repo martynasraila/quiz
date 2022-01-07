f005d0bfb9f8614d62177e2db0900b1c14d7de38
martynasraila <railamartynas@gmail.com>
2022-01-07 16:54:49 +0200

    Added array shuffle method

--- src/API.js
+++ src/API.js
@@ -1,6 +1,7 @@
 
 
 export const fetchQuizQuestions = async (amount, difficulty) => {
-    const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}`
-    const data = await (await fetch())
+    const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}`;
+    const data = await (await fetch(endpoint)).json();
+    return data.results
 }
