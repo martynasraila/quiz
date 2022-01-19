a35e458db3beead6a562472725eb29d9bdea4e55
martynasraila <railamartynas@gmail.com>
2022-01-19 12:09:53 +0200

    Added HE library for decoding HTML entities

--- src/API.js
+++ src/API.js
@@ -1,27 +1,28 @@
 import createRandom from "./Utilities";
+// library for decoding html entities
+import he from "he";
 
-export const fetchQuizQuestions = async (amount, difficulty) => {
-	const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}`;
+export const fetchQuizQuestions = async (amount, difficulty, category) => {
+	// check if the category is random, if its not then add string to api endpoint
+	const categoryStr = category.id !== -1 ? `&category=${category.id}` : "";
+	const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}${categoryStr}`;
 	const data = await (await fetch(endpoint)).json();
 	return data.results.map((question) => ({
 		...question,
 		answers: createRandom([
-			...question.incorrect_answers,
-			question.correct_answer
+			...question.incorrect_answers.map((incorrect) =>
+				he.decode(incorrect)
+			),
+			he.decode(question.correct_answer),
 		]),
-        question: question.question.replace(/&quot;/g, '\"').replace(/&#039;/g, "\'")
+		question: he.decode(question.question),
 	}));
 };
+// .replaceAll(/&quot;/g, '\"').replaceAll(/&#039;/g, "\'").replaceAll('&amp;', '&')
 
 export const fetchCategories = async () => {
-    const endPoint = "https://opentdb.com/api_category.php"
-    const data = await (await fetch(endPoint)).json();
-    return data.trivia_categories;
-}
+	const endPoint = "https://opentdb.com/api_category.php";
+	const data = await (await fetch(endPoint)).json();
+	return data.trivia_categories;
+};
 
-// export async function fetchCategories() {
-//     const endPoint = "https://opentdb.com/api_category.php";
-//     const data = await (await fetch(endPoint)).json();
-//     // console.log(data.trivia_categories);
-//     setCategories(data.trivia_categories.map(({name}) => ({id: name, name: name})));
-// }
